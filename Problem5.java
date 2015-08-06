29a7a34f429140ca4be645ab9bc2f40e1d976233
jankiel7410 <michal7410@gmail.com>
2015-08-06 10:12:24 +0200

    Created Problem5.java

---
+++ Problem5.java
@@ -0,0 +1,27 @@
+/*
+Program ma narysować tabliczkę mnożenia do 12. 
+Taką jak tu http://www.vaughns-1-pagers.com/computer/multiplication-tables/times-table-12x12.gif
+Tabele dwuwymiarowe definiujemy jako np. int[][], czyli po prostu tablica tablic.
+
+Trzeba przerobić metodę timesTable, bo aktualnie zwraca same zera.
+
+*/
+import java.util.Arrays;  //import klasy, która pozwoli mi wypisać tablicę
+
+class Problem5 {
+    public void timesTable() {
+        int n = 12;
+        int[][] table = int[16][16];
+        
+        //implement here
+        
+        return table;
+    }
+    
+    public void draw() {
+        int[][] table = timesTable();
+        for (int i = 0; i < table.length; i++) {    
+            System.out.println(Arrays.toString(table[i])); //nowy wiersz
+        }
+    }
+}
