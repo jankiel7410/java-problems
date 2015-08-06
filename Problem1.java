089502e105f5465b58d77af09d6b7a9759d004fd
jankiel7410 <michal7410@gmail.com>
2015-08-06 09:22:57 +0200

    created Problem1.java

---
+++ Problem1.java
@@ -0,0 +1,10 @@
+/*
+Napisz program, który pyta Cię o imię i odpowiada powitaniem. 
+Normalnie wita "Hello!", ale jeżeli imię to Alice lub Bob, to wita np. "Hello, Alice!"
+Brotip: http://javastart.pl/static/podstawy-jezyka/podstawowe-wejscie-wyjscie/
+*/
+public class Problem1{
+     public void greet(String name){
+         
+     }
+}
