d61a44a1d2a3fe932caa7a33234d3b89f66b8055
jankiel7410 <michal7410@gmail.com>
2015-08-06 09:27:59 +0200

    Created Problem2.java

---
+++ Problem2.java
@@ -0,0 +1,10 @@
+/*
+Dla parametru n zwróć sumę od 1 do n, ale tylko liczby podzielne przez 3 lub 5.
+Czyli dla n=8 wynik to 3+6+5=14
+*/
+
+class Problem2 {
+    public int sumWithDividers(int n) {
+        
+    }
+}
