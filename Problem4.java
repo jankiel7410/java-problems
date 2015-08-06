89ab24b09f7e2c1cc516f2ef8bc2f901e058f7f1
jankiel7410 <michal7410@gmail.com>
2015-08-06 09:52:35 +0200

    Created Problem4.java

---
+++ Problem4.java
@@ -0,0 +1,12 @@
+/*
+Napisz program, który odwraca zadany napis, np. "pies" -> "seip"
+Przypominam, przydatne metody: http://docs.oracle.com/javase/8/docs/api/java/lang/String.html#charAt-int- 
+oraz
+http://docs.oracle.com/javase/8/docs/api/java/lang/String.html#length--
+
+*/
+public class Problem4 {
+    public String reverse (String str) {
+        
+    }
+}
